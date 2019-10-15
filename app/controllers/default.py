575a6442dee4de11caf81541bb2ea2a0318937e0
Arquimedes <arquimedeslqjunior@gmail.com>
2019-10-15 00:59:55 -0300

    Criação de novos Tampletes e melhoria de usabilidade

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -5,3 +5,8 @@ from app import app
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/login")
+@app.route("/")
+def login():
+    return render_template('base.html')
